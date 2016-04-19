1ee9b878563c0cd348954d488252ce762fa101cb
Donny Davis <donnywdavis@icloud.com>
2016-04-19 15:36:15 -0400

    Set up initial logic for the calculator functionality

--- Calculator/Calculator/ViewController.m
+++ Calculator/Calculator/ViewController.m
@@ -7,8 +7,23 @@
 //
 
 #import "ViewController.h"
+#import "CalculatorBrain.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    
+    CalculatorBrain *brain;
+    
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *displayLabel;
+
+- (IBAction)operandTapped:(UIButton *)sender;
+- (IBAction)additionTapped:(UIButton *)sender;
+- (IBAction)subtractionTapped:(UIButton *)sender;
+- (IBAction)multiplicationTapped:(UIButton *)sender;
+- (IBAction)divisionTapped:(UIButton *)sender;
+- (IBAction)equalTapped:(UIButton *)sender;
+- (IBAction)allClearTapped:(UIButton *)sender;
 
 @end
 
@@ -17,11 +32,75 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.displayLabel.text = @"0";
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)operandTapped:(UIButton *)sender {
+    if (!brain) {
+        brain = [[CalculatorBrain alloc] init];
+    }
+    
+    if ([self.displayLabel.text containsString:@"."] && [sender.titleLabel.text isEqualToString:@"."]) {
+        return;
+    }
+    
+    if (brain.userIsTypingANumber) {
+        self.displayLabel.text = @"";
+        brain.userIsTypingANumber = NO;
+    }
+
+    if ([self.displayLabel.text isEqualToString:@"0"] && ![sender.titleLabel.text isEqualToString:@"."]) {
+        self.displayLabel.text = sender.titleLabel.text;
+    } else {
+        self.displayLabel.text = [self.displayLabel.text stringByAppendingString:sender.titleLabel.text];
+    }
+}
+
+- (IBAction)additionTapped:(UIButton *)sender {
+    brain.operatorType = OperatorTypeAddition;
+    brain.operand1 = [self.displayLabel.text floatValue];
+    brain.operand1String = [self.displayLabel.text mutableCopy];
+    brain.userIsTypingANumber = YES;
+}
+
+- (IBAction)subtractionTapped:(UIButton *)sender {
+    brain.operatorType = OperatorTypeSubtraction;
+    brain.operand1 = [self.displayLabel.text floatValue];
+    brain.operand1String = [self.displayLabel.text mutableCopy];
+    brain.userIsTypingANumber = YES;
+}
+
+- (IBAction)multiplicationTapped:(UIButton *)sender {
+    brain.operatorType = OperatorTypeMultiplication;
+    brain.operand1 = [self.displayLabel.text floatValue];
+    brain.operand1String = [self.displayLabel.text mutableCopy];
+    brain.userIsTypingANumber = YES;
+}
+
+- (IBAction)divisionTapped:(UIButton *)sender {
+    brain.operatorType = OperatorTypeDivision;
+    brain.operand1 = [self.displayLabel.text floatValue];
+    brain.operand1String = [self.displayLabel.text mutableCopy];
+    brain.userIsTypingANumber = YES;
+}
+
+- (IBAction)equalTapped:(UIButton *)sender {
+    brain.operand2 = [self.displayLabel.text floatValue];
+    brain.operand2String = [self.displayLabel.text mutableCopy];
+    self.displayLabel.text = [brain executeOperationOnOperands];
+    brain = [[CalculatorBrain alloc] init];
+}
+
+- (IBAction)allClearTapped:(UIButton *)sender {
+    self.displayLabel.text = @"0";
+    // Reset the brain
+    brain = [[CalculatorBrain alloc] init];
 }
 
 @end
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Homework 05
 
 ## Calculator
+
+### Issues
+* [ ] Number is appending to previous result after equal is pressed.
+* [ ] Invalid is returned on first run if no first number is selected.
+* [ ] After an operator is tapped and press "." it is not prefixed with 0.
+* [ ] Foramtting of result.
+
+### Solutions
+* [ ] In operandTapped check that OperatorType has been set to a value other than OperatorTypeNone.
+* [ ] Create a new brain in viewDidLoad instead of operandTapped so that we have a brain object to perform operations against.
+* [ ] Set userIsTypingANumber to YES once an operator is pressed. In operandTapped if userIsTypingANumber is YES then reset the label to 0 so that it will be appended with a decimal point.
+* [ ] If neither operand has a decimal then display result as int. If either operand has a decimal then format the result with at least 2-4 decimals.
